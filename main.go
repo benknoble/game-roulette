a80a62d865de5f29cdaa34a295fc34e590487f05
David Ben Knoble <ben.knoble@gmail.com>
2018-03-09 19:09:56 +0100

    Hand interrupts more gracefully

--- main.go
+++ main.go
@@ -2,13 +2,39 @@ package main
 
 import (
 	"log"
+	"os"
+	"os/signal"
 
 	"github.com/benknoble/game-roulette/app"
 	"github.com/benknoble/game-roulette/data"
 	"github.com/benknoble/game-roulette/web"
 )
 
+var (
+	hostname string
+)
+
+func init() {
+	go func() {
+		//Capture program shutdown, to make sure
+		//everything shuts down nicely
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt)
+		for sig := range c {
+			if sig == os.Interrupt {
+				app.Halt("Roulette Web Server %s shutting down", hostname)
+			}
+		}
+	}()
+}
+
 func main() {
+	var err error
+	hostname, err = os.Hostname()
+	if err != nil {
+		app.Halt("Error getting hostname %s", err.Error())
+	}
+
 	// load configuration somehow
 	// consider https://github.com/timshannon/config
 	// or Viper https://github.com/spf13/viper
@@ -17,14 +43,14 @@ func main() {
 	appConf := app.DefaultConfig()
 	dataConf := data.DefaultConfig()
 
-	err := data.Init(dataConf)
+	err = data.Init(dataConf)
 	if err != nil {
-		log.Fatalf("Error initializing data layer: %s", err.Error())
+		app.Halt("Error initializing data layer: %s", err.Error())
 	}
 
 	err = app.Init(appConf)
 	if err != nil {
-		log.Fatalf("Error initializing app layer: %s", err.Error())
+		app.Halt("Error initializing app layer: %s", err.Error())
 	}
 
 	err = web.StartServer(webConf)
